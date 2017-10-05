b477e806c5df2b9e5bc0d1a0f7f167846a82bc9b
Thibaut Decherit <thibaut.decherit@gmail.com>
2017-10-06 01:41:04 +0200

    working genres query

--- categories/videogames_API.php
+++ categories/videogames_API.php
@@ -12,7 +12,7 @@ require '../vendor/autoload.php';
 // Create a client with a base URI
 $client = new GuzzleHttp\Client(['base_uri' => 'https://api-2445582011268.apicast.io/']);
 
-// Query value
+// Query game per name
 $game = 'transistor';
 
 // Query, returns a string
@@ -22,11 +22,20 @@ $response = $client->request('GET', 'games/?search=' . $game . '&fields=*',
 // Transforms the string into a multidimensional array
 $games = json_decode($response->getBody()->getContents());
 
-var_dump($games);
+//var_dump($games);
 
 // Echoes specific value (0 means first match from query, name returns the value stored at the line with the key 'name')
-echo $games[0]->name;
-
-echo $games[0]->cover->url;
+//echo $games[0]->name;
 
 
+// Query the first two genres using their id collected by the game query
+$genreID1 = $games[0]->genres[0];
+$genreID2 = $games[0]->genres[1];
+
+$response = $client->request('GET', 'genres/' . $genreID1 . ',' . $genreID2,
+    ['headers' => ['user-key' => 'fcd9d20d2b43b91df2d64f9518414e95'],]);
+
+$genreID = json_decode($response->getBody()->getContents());
+
+// Echoes the first two genres
+echo $genreID[0]->name . ' - ' . $genreID[1]->name;
