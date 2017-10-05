6eaefc3486de7f94355bb6b2785e0aaec382232b
Thibaut Decherit <thibaut.decherit@gmail.com>
2017-10-05 17:02:13 +0200

    installed guzzle

--- categories/videogames.php
+++ categories/videogames.php
@@ -5,3 +5,10 @@
  * Date: 03/10/17
  * Time: 09:50
  */
+
+require '../vendor/autoload.php';
+// Create a client with a base URI
+$client = new GuzzleHttp\Client([
+        'base_uri' => 'https://foo.com/api/',
+    ]
+);
