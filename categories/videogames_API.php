de0d0899a78b9a19e333d343975ffed4945462a6
Thibaut Decherit <thibaut.decherit@gmail.com>
2017-10-05 23:58:27 +0200

    removed unused src folder

--- categories/videogames_API.php
+++ categories/videogames_API.php
@@ -5,9 +5,10 @@
  * Date: 03/10/17
  * Time: 09:50
  */
-
+ini_set('xdebug.var_display_max_depth', 5);
+ini_set('xdebug.var_display_max_children', 256);
+ini_set('xdebug.var_display_max_data', 1024);
 require '../vendor/autoload.php';
-include '../src/functions.php';
 // Create a client with a base URI
 $client = new GuzzleHttp\Client(['base_uri' => 'https://api-2445582011268.apicast.io/']);
 
@@ -21,5 +22,11 @@ $response = $client->request('GET', 'games/?search=' . $game . '&fields=*',
 // Transforms the string into a multidimensional array
 $games = json_decode($response->getBody()->getContents());
 
+var_dump($games);
+
 // Echoes specific value (0 means first match from query, name returns the value stored at the line with the key 'name')
 echo $games[0]->name;
+
+echo $games[0]->cover->url;
+
+
