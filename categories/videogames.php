f33e6e6b8bc53f7301cf05bf69fc9e47f6e09104
Thibaut Decherit <thibaut.decherit@gmail.com>
2017-10-05 22:43:46 +0200

    working query with specific key

--- categories/videogames.php
+++ categories/videogames.php
@@ -7,11 +7,19 @@
  */
 
 require '../vendor/autoload.php';
+include '../src/functions.php';
 // Create a client with a base URI
 $client = new GuzzleHttp\Client(['base_uri' => 'https://api-2445582011268.apicast.io/']);
 
-$response = $client->request('GET', 'characters/1', ['headers' =>
-    ['user-key' => 'fcd9d20d2b43b91df2d64f9518414e95'],
-    ]);
+// Query value
+$game = 'transistor';
 
-var_dump($response->getBody()->getContents());
+// Query, returns a string
+$response = $client->request('GET', 'games/?search=' . $game . '&fields=*',
+    ['headers' => ['user-key' => 'fcd9d20d2b43b91df2d64f9518414e95'],]);
+
+// Transforms the string into a multidimensional array
+$games = json_decode($response->getBody()->getContents());
+
+// Echoes specific value (0 means first match from query, name returns the value stored at the line with the key 'name')
+echo $games[0]->name;
