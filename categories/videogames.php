ce5945b30eac0c36f9cc10ba43921d67ac497a8b
Thibaut Decherit <thibaut.decherit@gmail.com>
2017-10-05 18:13:43 +0200

    working query with ID

--- categories/videogames.php
+++ categories/videogames.php
@@ -8,7 +8,10 @@
 
 require '../vendor/autoload.php';
 // Create a client with a base URI
-$client = new GuzzleHttp\Client([
-        'base_uri' => 'https://foo.com/api/',
-    ]
-);
+$client = new GuzzleHttp\Client(['base_uri' => 'https://api-2445582011268.apicast.io/']);
+
+$response = $client->request('GET', 'characters/1', ['headers' =>
+    ['user-key' => 'fcd9d20d2b43b91df2d64f9518414e95'],
+    ]);
+
+var_dump($response->getBody()->getContents());
